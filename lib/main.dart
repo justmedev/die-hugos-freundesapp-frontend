a5c66c1c07fbd5f492602ffa7e06ee82e3acd052
justmedev <ilja.ragnar@gmail.com>
2025-08-14 17:48:16 +0200

    basic login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:diehugosapp/buildcontext.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
@@ -10,26 +11,22 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// Try changing this and hot reloading the application.
-    ///
     /// To create a custom theme:
     /// ```shell
     /// dart forui theme create [theme template].
     /// ```
-    final theme = FThemes.zinc.dark;
+    final theme = context.isDarkModeEnabled
+        ? FThemes.zinc.dark
+        : FThemes.zinc.light;
 
     return MaterialApp(
-      // TODO: replace with your application's supported locales.
-      supportedLocales: FLocalizations.supportedLocales,
+      supportedLocales: [/*Locale('en'), */ Locale('de', 'AT')],
       // TODO: add your application's localizations delegates.
       localizationsDelegates: const [...FLocalizations.localizationsDelegates],
       builder: (_, child) => FTheme(data: theme, child: child!),
       theme: theme.toApproximateMaterialTheme(),
       // You can also replace FScaffold with Material Scaffold.
-      home: const FScaffold(
-        // TODO: replace with your widget.
-        child: Example(),
-      ),
+      home: const FScaffold(child: Example()),
     );
   }
 }
@@ -42,19 +39,45 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
-  int _count = 0;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) => Center(
     child: Column(
       mainAxisSize: MainAxisSize.min,
-      spacing: 10,
+      spacing: 20,
       children: [
-        Text('Count: $_count'),
-        FButton(
-          onPress: () => setState(() => _count++),
-          suffix: const Icon(FIcons.chevronsUp),
-          child: const Text('Increase'),
+        Text("⬇️ Die Hugos ⬇️", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
+
+        FCard(
+          title: const Text('Anmelden 🔐'),
+          subtitle: const Text(
+            'Bei Passwortzurücksetzung bitte bei Ilja melden :)',
+          ),
+          child: Column(
+            spacing: 20,
+            children: [
+              Column(
+                spacing: 10,
+                children: [
+                  FTextField.email(
+                    controller: _emailController, // TextEditingController
+                  ),
+                  FTextField.password(
+                    controller: _passwordController, // TextEditingController
+                  ),
+                ],
+              ),
+
+              FButton(
+                onPress: () {},
+                // onPress: () => setState(() => _count++),
+                suffix: const Icon(FIcons.lock),
+                child: const Text('Anmelden'),
+              ),
+            ],
+          ),
         ),
       ],
     ),
