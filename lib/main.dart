00103c67c9e37133da6ee9f2082809f6901cb087
Ilja Busch <ilja.ragnar@gmail.com>
2026-04-24 08:52:07 +0200

    add getx as a dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import "package:diehugosapp/core/utils/buildcontext_extensions.dart";
-import "package:diehugosapp/presentation/routes/router.dart";
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:forui/forui.dart";
+import "package:get/get.dart";
 
 void main() {
   runApp(const Application());
@@ -30,13 +30,12 @@ class Application extends StatelessWidget {
         ? theme.touch
         : theme.desktop;
 
-    return MaterialApp.router(
+    return GetMaterialApp.router(
       supportedLocales: const [/*Locale('en'), */ Locale("de", "AT")],
       // TODO(justmedev): add your application's localizations delegates.
       localizationsDelegates: const [...FLocalizations.localizationsDelegates],
       builder: (_, child) => FTheme(data: platformSpecificTheme, child: child!),
       theme: platformSpecificTheme.toApproximateMaterialTheme(),
-      routerConfig: goRouter,
     );
   }
 }
